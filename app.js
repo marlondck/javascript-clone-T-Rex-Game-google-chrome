61174b7b71785d18922b3ab06fe1d6c7cac13012
marlondck <15050029+marlondck@users.noreply.github.com>
2021-10-30 14:10:35 -0400

    ação de pulo

--- app.js
+++ app.js
@@ -3,9 +3,35 @@ const dino = document.querySelector('.dino')
 
 const handleKeyUp = event => {
   // https://keycode.info/
-  // barra de espaço foi precionada?
-  console.log(event.keyCode === 32)
+  if(event.keyCode === 32) {
+    jump()
+  }
+  
 }
 
+const jump = () => {
+  let dinoPosition = 0;
+  
+  // set interval serve para que execute o codigo em um determinado perio de tempo
+  let sobePulando = setInterval(() => {
+    if(dinoPosition >= 150) {
+      clearInterval(sobePulando)
+
+      // descendo
+      let desceDoPulo = setInterval(() => {
+        if(dinoPosition <= 0){
+          clearInterval(desceDoPulo)
+        } else {
+          dinoPosition -= 20
+          dino.style.bottom = `${dinoPosition}px`
+        }
+      }, 10);
+    } else {
+      // subindo
+      dinoPosition += 20
+      dino.style.bottom = `${dinoPosition}px`
+    }
+  }, 20)
+}
 
 document.addEventListener('keyup', handleKeyUp)
